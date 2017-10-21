5150c3ef51aaec6514e1f40d08076ada62dfffae
Justin Walker <jrwalker314@yahoo.com>
2017-10-21 22:55:25 +0000

    1st attempt at getting user data w/ problems.

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,12 +1,20 @@
 require 'httparty'
+require 'json'
 
 class Kele
   include HTTParty
-  base_uri 'https://www.bloc.io/api/v1'
+
+  def initialize(email, password)
+    @base_uri = "https://www.bloc.io/api/v1"
+    response = self.class.post(@base_uri+"/sessions", body: { "email": email, "password": password})
+    puts response.code
+    @auth_token = response["auth_token"]
+  end
   
-  def initialize(e, p)
-    @auth = {email: e, password: p}
-    self.class.post('https://www.bloc.io/api/v1/sessions', @auth)
+  def get_me
+    response = self.class.get(@base_uri+"/users/me", headers: {"authorization": @auth_token})
+    body = JSON.parse(response.body)
+    puts body
   end
 
 end
