941ef9be8e2e114a9a2cadb99367ddbb7702279d
Justin Walker <jrwalker314@yahoo.com>
2017-10-23 20:54:16 +0000

    Access a mentors availability info.

--- lib/kele.rb
+++ lib/kele.rb
@@ -15,4 +15,13 @@ class Kele
     JSON.parse(response.body)
   end
 
+  def get_mentor_availability(mentor_id)
+    response = self.class.get(@base_uri+"/mentors/"+mentor_id+"/student_availability", headers: {"authorization": @auth_token})
+    JSON.parse(response.body)
+  end
+  
+  def get_my_mentor_availability
+    me = get_me
+    get_mentor_availability(me["current_enrollment"]["mentor_id"].to_s)
+  end
 end
