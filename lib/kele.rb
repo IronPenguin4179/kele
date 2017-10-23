afe079c879ad4ad162f384447637fab3819022d5
Justin Walker <jrwalker314@yahoo.com>
2017-10-23 22:17:56 +0000

    Retrieves roadmaps

--- lib/kele.rb
+++ lib/kele.rb
@@ -5,18 +5,17 @@ class Kele
   include HTTParty
 
   def initialize(email, password)
-    @base_uri = "https://www.bloc.io/api/v1"
-    response = self.class.post(@base_uri+"/sessions", body: { "email": email, "password": password })
+    response = self.class.post(url("/sessions"), body: { "email": email, "password": password })
     @auth_token = response["auth_token"]
   end
   
   def get_me
-    response = self.class.get(@base_uri+"/users/me", headers: {"authorization": @auth_token})
+    response = self.class.get(url("/users/me"), headers: {"authorization": @auth_token})
     JSON.parse(response.body)
   end
 
   def get_mentor_availability(mentor_id)
-    response = self.class.get(@base_uri+"/mentors/"+mentor_id+"/student_availability", headers: {"authorization": @auth_token})
+    response = self.class.get(url("/mentors/"+mentor_id+"/student_availability"), headers: {"authorization": @auth_token})
     JSON.parse(response.body)
   end
   
@@ -24,4 +23,15 @@ class Kele
     me = get_me
     get_mentor_availability(me["current_enrollment"]["mentor_id"].to_s)
   end
+  
+  def show_roadmap(roadmap_id)
+    response = self.class.get(url("/roadmaps/"+roadmap_id), headers: {"authorization": @auth_token})
+    JSON.parse(response.body)
+    
+  end
+  
+  private
+    def url(endpoint)
+      "https://www.bloc.io/api/v1"+endpoint
+    end
 end
